a231fff694499fb100300b9bee279d516909a3c9
zenuas@gmail.com <zenuas@gmail.com>
2025-07-20 00:21:43 +0900

    初版

---
+++ README.md
@@ -0,0 +1,29 @@
+# Mina [![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+BookmarkMenu is Chrome extension bookmark-menu from toolbar  
+
+## Description
+
+BookmarkMenuはブックマークをツールバーから表示するChrome拡張である  
+
+## Installation
+
+## Requirement
+
+* Google Chrome  
+
+## Author
+
+[zenuas@GitHub](https://github.com/zenuas)  
+
+## License
+
+icon licensed Apache License Version 2.0 for [google/material-design-icons](https://github.com/google/material-design-icons)  
+
+## Reference
+
+https://developer.chrome.com/docs/extensions/reference/api/bookmarks
+
+https://developer.chrome.com/docs/extensions/how-to/ui/favicons
+
+https://developer.chrome.com/docs/extensions/reference/api/tabs
---
+++ script.js
@@ -0,0 +1,39 @@
+
+window.addEventListener("load", () =>
+{
+	const body = document.querySelector("body");
+	
+	function processNodes(parent, nodes)
+	{
+		//console.log(nodes);
+		for(let i = 0; i < nodes.length; i++)
+		{
+			const node = nodes[i];
+			if (node.children)
+			{
+				processNodes(parent, node.children);
+				continue;
+			}
+			
+			const li = document.createElement("li");
+			li.appendChild(document.createTextNode(node.title));
+			const url = new URL(chrome.runtime.getURL("/_favicon/"));
+			url.searchParams.set("pageUrl", node.url);
+			url.searchParams.set("size", "24");
+			li.style.backgroundImage = `url(${url.toString()})`;
+			
+			li.onmousedown = (e) => {
+				chrome.tabs.create({ url: node.url, active: (e.button == 0) });
+				return false;
+			};
+			parent.appendChild(li);
+		}
+	}
+	
+	chrome.bookmarks.getTree(nodes => {
+		
+		const ul = document.createElement("ul");
+		processNodes(ul, nodes);
+		body.appendChild(ul);
+	});
+});
